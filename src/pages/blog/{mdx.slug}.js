233034b3ef2cd9e53275bd43501d2e5b4cd8b6c8
Andrew B <andrew.b@bluefinch.uk>
2022-08-29 22:16:07 +0100

    Added footer to blog and removed unused images

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { StaticImage } from 'gatsby-plugin-image'
 
 import Layout from '../../components/layout'
 
@@ -13,10 +14,52 @@ const BlogPost = ({ data }) => {
       <h2 className=" text-xs text-slate-200 font-normal text-center pb-1">
         Posted by {data.mdx.frontmatter.author} on {data.mdx.frontmatter.date}
       </h2>
-      <div class="m-4 p-4 rounded bg-white text-black">
-        <article className="prose prose-slate mx-auto">
-          <MDXRenderer>{data.mdx.body}</MDXRenderer>
-        </article>
+      <div className="m-4 p-4 rounded bg-white text-black">
+        <div className="max-w-2xl mx-auto">
+          <article className="prose prose-slate pb-6 mx-auto">
+            <MDXRenderer>{data.mdx.body}</MDXRenderer>
+          </article>
+
+          <Link to={'/blog'}>&larr; Back to blog index</Link>
+
+          <hr className="my-5" />
+        </div>
+
+        <div className="flex px-5 pb-2 text-slate-600 max-w-xl mx-auto">
+          <div className="hidden md:block pr-4 pt-2">
+            <StaticImage
+              className="w-24"
+              src="../../images/Roundel.png"
+              alt="CoderDojo Logo"
+            />
+          </div>
+          <div>
+            <p className="pb-4">
+              CoderDojo Bishops Cleeve is a free coding club for children,
+              located near Cheltenham in the UK. Find out more on our{' '}
+              <Link to={`/`}>home page.</Link>
+            </p>
+            <p>
+              For more club news and coding articles, follow us on{' '}
+              <a
+                href="https://twitter.com/CoderDojoBC"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Twitter
+              </a>{' '}
+              and{' '}
+              <a
+                href="https://www.facebook.com/BishopsCleeveCoderDojo/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Facebook
+              </a>
+              .
+            </p>
+          </div>
+        </div>
       </div>
     </Layout>
   )
